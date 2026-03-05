1b656ae4301d623d7415bdb6380fc1658dd767ed
admtzy <blackraven429@gmail.com>
2026-03-05 22:59:51 +0700

    firstds t

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,12 +20,15 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email'=>'required|email',
-            'password'=>'required|string'
+            'email' => 'required|email',
+            'password' => 'required|string'
         ]);
 
-        if(Auth::attempt($request->only('email','password')))
+        $user = User::where('email',$request->email)->first();
+
+        if($user && $user->password == $request->password)
         {
+            Auth::login($user);
             return redirect()->route('dashboard');
         }
 
@@ -50,7 +53,7 @@ class AuthController extends Controller
         $user = User::create([
             'name'=>$request->name,
             'email'=>$request->email,
-            'password'=>Hash::make($request->password),
+            'password'=>($request->password),
             'role'=>'member',
         ]);
 
